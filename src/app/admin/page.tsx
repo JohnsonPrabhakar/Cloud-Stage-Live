b598d267fae0ad36f24df78d6cab47874f557498
John Kennady_official <j.johny.111@gmail.com>
2025-07-16 12:49:21 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <Primitive.div rol

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -14,6 +14,7 @@ import {
   DialogHeader,
   DialogTitle,
   DialogTrigger,
+  DialogClose
 } from "@/components/ui/dialog"
 import Image from 'next/image';
 import { Badge } from '@/components/ui/badge';
